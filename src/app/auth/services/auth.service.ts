fd506cedcd9513db6f73e8a06f109cdeac41ffe9
OPOII <bryancamili198@hotmail.com>
2023-09-04 17:23:22 -0500

    login-post

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 import { environment } from 'src/environments/environments';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +12,28 @@ export class AuthService {
   private readonly baseUrl:string=environment.baseUrl;
   private http=inject(HttpClient);
 
+  // Para que nadie cambie la señal
   private _currenUser=signal<User|null>(null);
   private _authStatus=signal<AuthStatus>(AuthStatus.checking);
 
+  //! Al mundo exterior
+  // Así nadie va a cambiar los estados
+  public currentUser=computed(()=>this._currenUser());
+  public authStatus=computed(()=>this._authStatus());
   constructor() { }
 
-  login(emai:string,password:string):Observable<boolean>{
-    return of(true);
+  login(email:string,password:string):Observable<boolean>{
+    const url=`${this.baseUrl}/auth/login`
+    const body={email,password};
+    return this.http.post<LoginResponse>(url,body)
+    .pipe(
+      tap(({user,token})=>{
+        this._currenUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token',token);
+      }),
+      map(()=>true)
+      //Todo: errores
+    );
   }
 }
