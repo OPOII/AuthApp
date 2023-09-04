25976ee6f43eb29c806c46db295fbc3a5e9a8f1b
OPOII <bryancamili198@hotmail.com>
2023-09-04 16:28:25 -0500

    tipos e interfaces

---
+++ src/app/auth/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from './aut-status.enum'
+export * from './login-response.interface'
+export * from './user.interface'
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@angular/core';
+import { AuthStatus, User } from '../interfaces';
 import { environment } from 'src/environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +10,14 @@ import { environment } from 'src/environments/environments';
 export class AuthService {
 
   private readonly baseUrl:string=environment.baseUrl;
+  private http=inject(HttpClient);
+
+  private _currenUser=signal<User|null>(null);
+  private _authStatus=signal<AuthStatus>(AuthStatus.checking);
 
   constructor() { }
+
+  login(emai:string,password:string):Observable<boolean>{
+    return of(true);
+  }
 }
