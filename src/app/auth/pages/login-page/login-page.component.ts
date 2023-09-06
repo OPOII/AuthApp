6f588450b48e0c9aad88df9fd151a9eb6d53dc12
OPOII <bryancamili198@hotmail.com>
2023-09-06 10:34:18 -0500

    function guards

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Component({
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
@@ -10,16 +11,19 @@ export class LoginPageComponent {
 
   private fb=inject(FormBuilder);
   private authService=inject(AuthService);
+  private router=inject(Router);
+
+
   public myForm:FormGroup=this.fb.group({
-    email:['',[Validators.required,Validators.email]],
-    password:['',[Validators.required,Validators.minLength(6)]],
+    email:['bryangrueso2@gmail.com',[Validators.required,Validators.email]],
+    password:['z99817omega',[Validators.required,Validators.minLength(6)]],
   })
 
   login(){
     const {email,password}=this.myForm.value;
     this.authService.login(email,password)
     .subscribe({
-      next:()=>console.log(),
+      next:()=>this.router.navigateByUrl('/dashboard'),
       error:(message)=>{
         Swal.fire('Error',message,'error')
         console.log({loginError:message});
